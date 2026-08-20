4cb8e2b20021724a966d62ebdf133f682464f093
Nashin <nashin1234567890@gmail.com>
2026-08-20 06:42:44 +0500

    fix: rebuild node_modules for clean install

--- next.config.js
+++ next.config.js
@@ -20,7 +20,6 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
