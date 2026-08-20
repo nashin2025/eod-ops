01d47a33e0aee5436c71dd317e0f8f0b14dd3065
Nashin <nashin1234567890@gmail.com>
2026-08-20 06:56:30 +0500

    vercel: add output: standalone for proper server file tracing

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
